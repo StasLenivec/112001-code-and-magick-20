78027974a92ec8a778a575b005f09631c7447676
Стас Ленивец <rostovs154@gmail.com>
2020-06-02 16:43:14 +0700

    Добавил рандом цветов для статистики

--- js/stat.js
+++ js/stat.js
@@ -3,12 +3,12 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
-var CLOUD_X = 100; //расположение облака по Х
-var CLOUD_Y = 10; //расположение облака по У
-var GAP = 50; //отступ между колонками
-var FONT_GAP = 15; //размер шрифта
-var BAR_HEIGHT = 150; //высота колонки
-var barWidth = 40;//ширина колонки
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 50;
+var FONT_GAP = 15;
+var BAR_HEIGHT = 150;
+var barWidth = 40;
 var CONGRATS_TEXT_X = 125;
 var CONGRATS_TEXT_Y = 30;
 
@@ -26,9 +26,15 @@ var getMaxElement = function(arr) {
       maxElement = arr[i];
     }
   }
-
   return maxElement;
+};
 
+var getBarColor = function (name) {
+  var barColor = 'hsl(240, 100%, ' + Math.floor(Math.random() * 50) + '%)';
+  if (name === 'Вы') {
+    barColor = 'rgba(255, 0, 0, 1)';
+  }
+  return barColor;
 };
 
 window.renderStatistics = function(ctx, names, times) {
@@ -44,13 +50,8 @@ window.renderStatistics = function(ctx, names, times) {
   ctx.fillText('Список результатов:', CONGRATS_TEXT_X, CONGRATS_TEXT_Y + 20);
 
   for (var i = 0; i < names.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsl(240, 100%, 50%)';
-    }
+    ctx.fillStyle = getBarColor(names[i]);
     ctx.fillRect(CLOUD_X + GAP * (i * 2 + 1), barWidth * 2, barWidth, (BAR_HEIGHT * times[i]) / maxTime);
-
     ctx.fillStyle = '#000';
     ctx.fillText(names[i], CLOUD_X + GAP * (i * 2 + 1), CLOUD_X + BAR_HEIGHT);
     ctx.fillText(Math.round(times[i]), CLOUD_X + GAP * (i * 2 + 1), CLOUD_Y * 2 + GAP );
